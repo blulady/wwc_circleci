9674b63c477c59da711a1c6cab35e15760ac458d
Jai Purandare <jai2005@gmail.com>
2020-09-15 16:38:46 -0700

    modified the view to use Userprofile and RegistrationToken models. Basic working view

--- api/wwcodesvtools/api/views.py
+++ api/wwcodesvtools/api/views.py
@@ -13,25 +13,23 @@ class UserRegistrationView(APIView):
     def post(self, request, format=None):
         req = request.data
         user_queryset = User.objects.select_related('userprofile').filter(email=req['user']['email'])
-        data = None
         res_status = None
         error = None
         try:
             if user_queryset.exists():
                 user = user_queryset.first()
-                user_status = UserProfile.objects.filter(user = user).order_by('created_at').last()
-                if user_status != UserProfile.NEW:
+                if not user.userprofile.is_new():
                     res_status = status.HTTP_400_BAD_REQUEST
                     raise Exception('User already registered')
-                token = RegistrationToken.objects.filter(user = user, used=False)
-                if token != req['token']:
+                token = RegistrationToken.objects.get(token=req['token'])
+                if not token or token.user.email != user.email:
                     res_status = status.HTTP_400_BAD_REQUEST
                     raise Exception('Invalid token. Token Does not match the token generated for this user.')
                 serializer = UserSerializer(user,data=req['user'])
                 if serializer.is_valid():
                     serializer.save()
-                    UserProfile.objects.create(user=user, status= UserProfile.ACTIVE)
-                    data = serializer.data
+                    user.userprofile.status = UserProfile.ACTIVE
+                    user.userprofile.save(update_fields=['status'])
                     res_status = status.HTTP_201_CREATED
                 else: 
                     error = "Invalid Data"
@@ -40,10 +38,9 @@ class UserRegistrationView(APIView):
                 error = "User not created"
                 res_status = status.HTTP_404_NOT_FOUND
         except Exception as e:
-            error = "Something went wrong", "{0}".format(e)
+            error = "Something went wrong : {0}".format(e)
             res_status = res_status or status.HTTP_500_INTERNAL_SERVER_ERROR
-        data = data or {'error': error}
-        return Response(data, status=res_status)
+        return Response({'error': error}, status=res_status)
     
 
 

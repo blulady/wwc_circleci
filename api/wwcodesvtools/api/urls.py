701f46f280c4499417fae8521dc1d21b6a1c2ade
Jai Purandare <jai2005@gmail.com>
2020-09-20 02:32:02 -0700

    Removed commented out code

--- api/wwcodesvtools/api/urls.py
+++ api/wwcodesvtools/api/urls.py
@@ -3,10 +3,6 @@ from rest_framework import routers
 
 from .views import UserRegistrationView
 
-# router = routers.DefaultRouter()
-# router.register("register", views.UserRegistrationView)
-
 urlpatterns = [
     path("registration/", UserRegistrationView.as_view()),
-    # path("registration/", include(router.urls)),
 ]

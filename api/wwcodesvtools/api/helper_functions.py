ed21cf5232ef09e95220341999ee2b3530dc5750
Anita B <56373303+write2anitab@users.noreply.github.com>
2020-10-04 19:30:37 -0700

    added Django default core EmailValidator (#109)

--- api/wwcodesvtools/api/helper_functions.py
+++ api/wwcodesvtools/api/helper_functions.py
@@ -3,6 +3,7 @@ from django.template.loader import get_template
 from django.core.mail import EmailMessage
 from django.conf import settings
 from smtplib import SMTPException
+from django.core.validators import validate_email, ValidationError
 import logging
 
 logger = logging.getLogger('django')
@@ -10,14 +11,14 @@ logger = logging.getLogger('django')
 def send_email_helper(to_email, subject, template_file, context_data):
     logger.debug(f"send_email_helper: to_email: {to_email} subject: {subject} template_file: {template_file} contextData: {context_data}")
     if subject and template_file and to_email and context_data:
-        from_email = settings.EMAIL_HOST_USER
-        message = get_template(template_file).render(context_data)
-        msg = EmailMessage(subject, message, from_email, [to_email])
-        msg.content_subtype = "html"
-
         try:
+            validate_email(to_email)
+            from_email = settings.EMAIL_HOST_USER
+            message = get_template(template_file).render(context_data)
+            msg = EmailMessage(subject, message, from_email, [to_email])
+            msg.content_subtype = "html"
             msg.send()
-        except (ValueError, SMTPException) as e:
+        except (ValidationError,ValueError, SMTPException) as e:
             logger.error(f"send_email_helper: There was an error sending an email: {e}")
             return False
         else:

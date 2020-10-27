4b817765f1b945c171ecf497f30031937e8ce4dd
divya1c <divya@oneconcern.com>
2020-10-26 17:43:03 -0700

    cleaning up - removing backend training code from the repo (#128)

--- backend_training/mysite/myapp/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from myapp.models import Role,UserProfile
-
-# Register your models here.
-admin.site.register(Role)
-admin.site.register(UserProfile)
--- backend_training/mysite/myapp/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls import url
-from myapp import views
-
-app_name = 'myapp'
-
-urlpatterns = [
-    url(r'^user_auth/$',views.UserAuthView.as_view(),name='UserAuthView'),
-]

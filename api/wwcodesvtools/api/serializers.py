d2870972419b3b61d2e15f107629e39c6f37d9a4
Margarita89 <62974332+Margarita89@users.noreply.github.com>
2020-10-27 16:42:44 -0700

    Validation of serializer added for the user registration (#123)
    * validation of serializer added for the user registration
    
    * Updated with use of regex for password requirements
    
    @jaipurandare @divya1c: @Margarita89 and I have updated with replacing the for loop with the use of regex instead. Please let us know your feedback, thank you.
    
    * Minor grammatical corrections
    
    Co-authored-by: rishikasb <38222778+rishikasb@users.noreply.github.com>
    Co-authored-by: Jai Purandare <jai2005@gmail.com>

--- api/wwcodesvtools/api/serializers.py
+++ api/wwcodesvtools/api/serializers.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.models import User
 from django.contrib.auth.hashers import make_password
 from rest_framework import serializers
+import re
 
 
 class UserRegistrationSerializer(serializers.ModelSerializer):
@@ -17,6 +18,40 @@ class UserRegistrationSerializer(serializers.ModelSerializer):
         instance.password = make_password(validated_data.get('password', instance.password))
         instance.save()
         return instance
+
+    def validate_first_name(self, value):
+        """
+        Check that the first_name is not None or empty.
+        """
+        if not value:
+            raise serializers.ValidationError("First name should not be empty or None")
+        return value
+
+    def validate_last_name(self, value):
+        """
+        Check that the last_name is not None or empty.
+        """
+        if not value:
+            raise serializers.ValidationError("Last name should not be empty or None")
+        return value
+
+    def validate_password(self, value):
+        """
+        Check that the password is correct:
+            8-50 characters.
+            At least one upper case letter
+            At least one lower case letter
+            At least one numeric char
+        """
+        if not (8 <= len(value) <= 50):
+            raise serializers.ValidationError("Password should be 8 to 50 characters long")
+        if not re.search(r'[A-Z]', value):
+            raise serializers.ValidationError("Password should have at least one upper case letter")
+        if not re.search(r'[a-z]', value):
+            raise serializers.ValidationError("Password should have at least one lower case letter")
+        if not re.search(r'\d', value):
+            raise serializers.ValidationError("Password should have at least one number")
+        return value
 
 
 class MailSenderSerializer(serializers.Serializer):

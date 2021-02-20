e3e0b590ee510a71adec3e4a2979aaf3153722a6
Maria Goretti <goretti_rivera@hotmail.com>
2021-02-19 17:36:30 -0800

    Sorting members issue# 135 (#208)
    * adding sorting
    
    * modifying get member data fixture
    
    * fixed test cases due to the get members data fixture change
    
    * adding unit testing
    
    * adding swagger desc

--- api/wwcodesvtools/api/tests.py
+++ api/wwcodesvtools/api/tests.py
@@ -556,38 +556,38 @@ class TestGetMembersView(TransactionTestCase):
     # Testing get members with role = DIRECTOR
     # 'PENDING' status members and 'email' field are in the response
     def test_get_members_role_director(self):
-        self.username = 'UserDirector@example.com'
-        self.password = 'Password1@'
+        self.username = 'director@example.com'
+        self.password = 'Password123'
         access_token = self.get_token(self.username, self.password)
         bearer = {'HTTP_AUTHORIZATION': 'Bearer {}'.format(access_token)}
         response = self.client.get("/api/users/", **bearer)
         responseLength = len(response.data)
         self.assertEqual(responseLength, 4)
         self.assertEqual(json.loads(response.content)[0]['id'], 4)
-        self.assertEqual(json.loads(response.content)[0]['email'], 'UserLeaderPending@example.com')
-        self.assertEqual(json.loads(response.content)[0]['first_name'], 'new')
-        self.assertEqual(json.loads(response.content)[0]['last_name'], 'user')
+        self.assertEqual(json.loads(response.content)[0]['email'], 'leaderPendingStatus@example.com')
+        self.assertEqual(json.loads(response.content)[0]['first_name'], 'Caroline')
+        self.assertEqual(json.loads(response.content)[0]['last_name'], 'Miller')
         self.assertEqual(json.loads(response.content)[0]['userprofile']['status'], 'PENDING')
         self.assertEqual(json.loads(response.content)[0]['userprofile']['role'], 'LEADER')
-        self.assertEqual(json.loads(response.content)[0]['date_joined'], '2020-12-14T20:26:55.902000Z')
+        self.assertEqual(json.loads(response.content)[0]['date_joined'], '2021-02-19T01:56:51.160000Z')
         for i in range(responseLength):
             self.assertIsNotNone(json.loads(response.content)[i]['email'])
 
     # Testing get members with role = VOLUNTEER
     # 'PENDING' status members and 'email' field not in the response
     def test_get_members_role_volunteer(self):
-        self.username = 'UserVolunteer@example.com'
-        self.password = 'Password1@'
+        self.username = 'volunteer@example.com'
+        self.password = 'Password123'
         access_token = self.get_token(self.username, self.password)
         bearer = {'HTTP_AUTHORIZATION': 'Bearer {}'.format(access_token)}
         response = self.client.get("/api/users/", **bearer)
         responseLength = len(response.data)
         self.assertEqual(responseLength, 3)
         self.assertEqual(json.loads(response.content)[0]['id'], 3)
-        self.assertEqual(json.loads(response.content)[0]['first_name'], 'User FirstName')
-        self.assertEqual(json.loads(response.content)[0]['last_name'], 'User LastName')
+        self.assertEqual(json.loads(response.content)[0]['first_name'], 'Bruno')
+        self.assertEqual(json.loads(response.content)[0]['last_name'], 'Clark')
         self.assertEqual(json.loads(response.content)[0]['userprofile']['role'], 'LEADER')
-        self.assertEqual(json.loads(response.content)[0]['date_joined'], '2020-12-14T20:13:39.660000Z')
+        self.assertEqual(json.loads(response.content)[0]['date_joined'], '2021-02-19T01:56:29.756000Z')
         self.assertEqual(json.loads(response.content)[0]['userprofile']['status'], 'ACTIVE')
         for i in range(responseLength):
             self.assertRaises(KeyError, lambda: json.loads(response.content)[i]['email'])
@@ -596,18 +596,18 @@ class TestGetMembersView(TransactionTestCase):
     # Testing get members with role = LEADER
     # 'PENDING' status members and 'email' field not in the response
     def test_get_members_role_leader(self):
-        self.username = 'UserLeader@example.com'
-        self.password = 'Password1@'
+        self.username = 'leader@example.com'
+        self.password = 'Password123'
         access_token = self.get_token(self.username, self.password)
         bearer = {'HTTP_AUTHORIZATION': 'Bearer {}'.format(access_token)}
         response = self.client.get("/api/users/", **bearer)
         responseLength = len(response.data)
         self.assertEqual(responseLength, 3)
         self.assertEqual(json.loads(response.content)[0]['id'], 3)
-        self.assertEqual(json.loads(response.content)[0]['first_name'], 'User FirstName')
-        self.assertEqual(json.loads(response.content)[0]['last_name'], 'User LastName')
+        self.assertEqual(json.loads(response.content)[0]['first_name'], 'Bruno')
+        self.assertEqual(json.loads(response.content)[0]['last_name'], 'Clark')
         self.assertEqual(json.loads(response.content)[0]['userprofile']['role'], 'LEADER')
-        self.assertEqual(json.loads(response.content)[0]['date_joined'], '2020-12-14T20:13:39.660000Z')
+        self.assertEqual(json.loads(response.content)[0]['date_joined'], '2021-02-19T01:56:29.756000Z')
         self.assertEqual(json.loads(response.content)[0]['userprofile']['status'], 'ACTIVE')
         for i in range(responseLength):
             self.assertRaises(KeyError, lambda: json.loads(response.content)[i]['email'])
@@ -628,24 +628,24 @@ class TestGetMemberInfoView(TransactionTestCase):
 
     # Testing get member info with role = DIRECTOR
     def test_get_member_info_for_director(self):
-        self.username = 'UserDirector@example.com'
-        self.password = 'Password1@'
+        self.username = 'director@example.com'
+        self.password = 'Password123'
         access_token = self.get_token(self.username, self.password)
         bearer = {'HTTP_AUTHORIZATION': 'Bearer {}'.format(access_token)}
         response = self.client.get("/api/user/1", **bearer)
         self.assertEqual(json.loads(response.content)['id'], 1)
-        self.assertEqual(json.loads(response.content)['email'], 'UserDirector@example.com')
-        self.assertEqual(json.loads(response.content)['first_name'], 'User FirstName')
-        self.assertEqual(json.loads(response.content)['last_name'], 'User LastName')
+        self.assertEqual(json.loads(response.content)['email'], 'director@example.com')
+        self.assertEqual(json.loads(response.content)['first_name'], 'John')
+        self.assertEqual(json.loads(response.content)['last_name'], 'Smith')
         self.assertEqual(json.loads(response.content)['userprofile']['status'], 'ACTIVE')
         self.assertEqual(json.loads(response.content)['userprofile']['role'], 'DIRECTOR')
-        self.assertEqual(json.loads(response.content)['date_joined'], '2020-12-14T20:13:19.823000Z')
+        self.assertEqual(json.loads(response.content)['date_joined'], '2021-02-19T01:55:01.810000Z')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
     # Testing get member info with role = LEADER
     def test_get_member_info_for_leader(self):
-        self.username = 'UserLeader@example.com'
-        self.password = 'Password1@'
+        self.username = 'leader@example.com'
+        self.password = 'Password123'
         access_token = self.get_token(self.username, self.password)
         bearer = {'HTTP_AUTHORIZATION': 'Bearer {}'.format(access_token)}
         response = self.client.get("/api/user/1", **bearer)
@@ -654,8 +654,8 @@ class TestGetMemberInfoView(TransactionTestCase):
 
     # Testing get member info with role = VOLUNTEER
     def test_get_member_info_for_volunteer(self):
-        self.username = 'UserVolunteer@example.com'
-        self.password = 'Password1@'
+        self.username = 'volunteer@example.com'
+        self.password = 'Password123'
         access_token = self.get_token(self.username, self.password)
         bearer = {'HTTP_AUTHORIZATION': 'Bearer {}'.format(access_token)}
         response = self.client.get("/api/user/2", **bearer)
@@ -691,3 +691,102 @@ class TestLogoutView(TransactionTestCase):
         self.assertEqual(refresh_response.status_code, status.HTTP_401_UNAUTHORIZED)
         self.assertEqual(json.loads(refresh_response.content)['detail'], "Token is blacklisted")
         self.assertEqual(json.loads(refresh_response.content)['code'], "token_not_valid")
+
+
+class TestGetMembersOrdering(TransactionTestCase):
+    reset_sequences = True
+    fixtures = ['get_members_data.json']
+
+    def get_token(self, username, password):
+        s = TokenObtainPairSerializer(data={
+            TokenObtainPairSerializer.username_field: self.username,
+            'password': self.password,
+        })
+        self.assertTrue(s.is_valid())
+        return s.validated_data['access']
+
+    # Testing get members ordering with role = DIRECTOR
+    # first_name field ordered by "Ascending" order
+    def test_get_members_ordering_by_first_name_asc(self):
+        self.username = 'director@example.com'
+        self.password = 'Password123'
+        access_token = self.get_token(self.username, self.password)
+        bearer = {'HTTP_AUTHORIZATION': 'Bearer {}'.format(access_token)}
+        response = self.client.get("/api/users/?ordering=first_name", **bearer)
+        responseLength = len(response.data)
+        self.assertEqual(responseLength, 4)
+        self.assertEqual(json.loads(response.content)[0]['first_name'], 'Alice')
+        self.assertEqual(json.loads(response.content)[1]['first_name'], 'Bruno')
+        self.assertEqual(json.loads(response.content)[2]['first_name'], 'Caroline')
+        self.assertEqual(json.loads(response.content)[3]['first_name'], 'John')
+
+    # Testing get members ordering with role = DIRECTOR
+    # first_name field ordered by "Descending" order
+    def test_get_members_ordering_by_first_name_desc(self):
+        self.username = 'director@example.com'
+        self.password = 'Password123'
+        access_token = self.get_token(self.username, self.password)
+        bearer = {'HTTP_AUTHORIZATION': 'Bearer {}'.format(access_token)}
+        response = self.client.get("/api/users/?ordering=-first_name", **bearer)
+        responseLength = len(response.data)
+        self.assertEqual(responseLength, 4)
+        self.assertEqual(json.loads(response.content)[0]['first_name'], 'John')
+        self.assertEqual(json.loads(response.content)[1]['first_name'], 'Caroline')
+        self.assertEqual(json.loads(response.content)[2]['first_name'], 'Bruno')
+        self.assertEqual(json.loads(response.content)[3]['first_name'], 'Alice')
+
+    # Testing get members ordering with role = LEADER
+    # last_name field ordered by "Ascending" order
+    def test_get_members_ordering_by_last_name_asc(self):
+        self.username = 'leader@example.com'
+        self.password = 'Password123'
+        access_token = self.get_token(self.username, self.password)
+        bearer = {'HTTP_AUTHORIZATION': 'Bearer {}'.format(access_token)}
+        response = self.client.get("/api/users/?ordering=last_name", **bearer)
+        responseLength = len(response.data)
+        self.assertEqual(responseLength, 3)
+        self.assertEqual(json.loads(response.content)[0]['last_name'], 'Clark')
+        self.assertEqual(json.loads(response.content)[1]['last_name'], 'Robinson')
+        self.assertEqual(json.loads(response.content)[2]['last_name'], 'Smith')
+
+    # Testing get members ordering with role = LEADER
+    # last_name field ordered by "Descending" order
+    def test_get_members_ordering_by_last_name_desc(self):
+        self.username = 'leader@example.com'
+        self.password = 'Password123'
+        access_token = self.get_token(self.username, self.password)
+        bearer = {'HTTP_AUTHORIZATION': 'Bearer {}'.format(access_token)}
+        response = self.client.get("/api/users/?ordering=-last_name", **bearer)
+        responseLength = len(response.data)
+        self.assertEqual(responseLength, 3)
+        self.assertEqual(json.loads(response.content)[0]['last_name'], 'Smith')
+        self.assertEqual(json.loads(response.content)[1]['last_name'], 'Robinson')
+        self.assertEqual(json.loads(response.content)[2]['last_name'], 'Clark')
+
+    # Testing get members ordering with role = VOLUNTEER
+    # date_joined field ordered by "Ascending" order
+    def test_get_members_ordering_by_date_joined_asc(self):
+        self.username = 'volunteer@example.com'
+        self.password = 'Password123'
+        access_token = self.get_token(self.username, self.password)
+        bearer = {'HTTP_AUTHORIZATION': 'Bearer {}'.format(access_token)}
+        response = self.client.get("/api/users/?ordering=date_joined", **bearer)
+        responseLength = len(response.data)
+        self.assertEqual(responseLength, 3)
+        self.assertEqual(json.loads(response.content)[0]['date_joined'], '2021-02-19T01:55:01.810000Z')
+        self.assertEqual(json.loads(response.content)[1]['date_joined'], '2021-02-19T01:56:06.115000Z')
+        self.assertEqual(json.loads(response.content)[2]['date_joined'], '2021-02-19T01:56:29.756000Z')
+
+    # Testing get members ordering with role = VOLUNTEER
+    # date_joined field ordered by "Descending" order
+    def test_get_members_ordering_by_date_joined_desc(self):
+        self.username = 'volunteer@example.com'
+        self.password = 'Password123'
+        access_token = self.get_token(self.username, self.password)
+        bearer = {'HTTP_AUTHORIZATION': 'Bearer {}'.format(access_token)}
+        response = self.client.get("/api/users/?ordering=-date_joined", **bearer)
+        responseLength = len(response.data)
+        self.assertEqual(responseLength, 3)
+        self.assertEqual(json.loads(response.content)[0]['date_joined'], '2021-02-19T01:56:29.756000Z')
+        self.assertEqual(json.loads(response.content)[1]['date_joined'], '2021-02-19T01:56:06.115000Z')
+        self.assertEqual(json.loads(response.content)[2]['date_joined'], '2021-02-19T01:55:01.810000Z')
--- api/wwcodesvtools/api/views/GetMembersView.py
+++ api/wwcodesvtools/api/views/GetMembersView.py
@@ -4,19 +4,29 @@ from rest_framework.permissions import IsAuthenticated
 from api.serializers import GetMemberForDirectorSerializer, GetMemberSerializer
 from api.helper_functions import is_director_or_superuser
 from api.permissions import CanGetMemberInfo
+from rest_framework.filters import OrderingFilter
 
 
 class GetMembersView(ListAPIView):
     """
     Returns a list of all members.
+
+    Ordering by first_name, last_name or date_joined
+    ------------------------------------------------
+    You may also specify reverse orderings by prefixing the field name with '-', like so:
+    http//example.com/api/users/?ordering=first_name
+    http//example.com/api/users/?ordering=-first_name
     """
     permission_classes = [IsAuthenticated]
+    filter_backends = [OrderingFilter]
+    ordering_fields = ['first_name', 'last_name', 'date_joined']
+    ordering = ['-date_joined']
 
     def get_queryset(self):
         if is_director_or_superuser(self.request.user.id, self.request.user.is_superuser):
-            queryset = User.objects.all().order_by('-date_joined')
-            return queryset
-        queryset = User.objects.exclude(userprofile__status='PENDING').order_by('-date_joined')
+            queryset = User.objects.all()
+        else:
+            queryset = User.objects.exclude(userprofile__status='PENDING')
         return queryset
 
     def get_serializer_class(self):

4a7270eeb19e534b67d692947226ffd6343baff0
Dorossi <dorossi.maroc@gmail.com>
2021-03-10 10:26:03 -0800

    edit member first version - some tests missings
    rebase done

---
+++ api/wwcodesvtools/api/tests/test_edit_member.py
@@ -0,0 +1,224 @@
+import json
+from unittest.mock import MagicMock
+from django.test import TestCase, override_settings, TransactionTestCase
+from django.db import IntegrityError
+from rest_framework import exceptions as drf_exceptions
+from rest_framework_simplejwt.serializers import TokenObtainSerializer, TokenObtainPairSerializer
+from rest_framework_simplejwt.tokens import AccessToken, RefreshToken
+from ..models import UserProfile, User, RegistrationToken, Team, User_Team
+from rest_framework import status
+
+
+
+class EditMemberViewTestCase(TransactionTestCase):
+    reset_sequences = True
+    fixtures = ['get_members_data.json']
+    EXPECTED_MESSAGE = 'User edited successfully'
+
+    def setUp(self):
+        self.username = 'director@example.com'
+        self.password = 'Password123'
+        self.access_token = self.get_token(self.username, self.password)
+        self.bearer = {
+            'HTTP_AUTHORIZATION': 'Bearer {}'.format(self.access_token)}
+
+    def get_token(self, username, password):
+        s = TokenObtainPairSerializer(data={
+            TokenObtainPairSerializer.username_field: username,
+            'password': password,
+        })
+        self.assertTrue(s.is_valid())
+        return s.validated_data['access']
+
+    def check_member_before_change(self, data):
+        # Before change
+        endpoint = "/api/user/" + str(data['user_id'])
+        response = self.client.get(endpoint, **self.bearer)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(json.loads(response.content)['id'], data['user_id'])
+        self.assertEqual(json.loads(response.content)[
+                         'email'], data['email'])
+        self.assertEqual(json.loads(response.content)[
+                         'first_name'], data['first_name'])
+        self.assertEqual(json.loads(response.content)[
+                         'last_name'], data['last_name'])
+        self.assertEqual(json.loads(response.content)[
+                         'status'], data['user_status'])
+        self.assertEqual(json.loads(response.content)['role'], data['role'])
+
+    def check_member_after_change(self, user_data):
+        endpoint = "/api/user/" + str(user_data['user_id'])
+        response = self.client.get(endpoint, **self.bearer)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(json.loads(response.content)
+                         ['id'], user_data["user_id"])
+        self.assertEqual(json.loads(response.content)[
+                         'email'], user_data["email"])
+        self.assertEqual(json.loads(response.content)[
+                         'first_name'], user_data["first_name"])
+        self.assertEqual(json.loads(response.content)[
+                         'last_name'], user_data["last_name"])
+        self.assertEqual(json.loads(response.content)[
+                         'status'], user_data["user_status"])
+        self.assertEqual(json.loads(response.content)
+                         ['role'], user_data["role"])
+    
+    def test_edit_member_nonexistant_id(self):
+        user_data = {
+            'user_id': 404,
+            'email':  'leaderPendingStatus@example.com',
+            'first_name': 'Caroline',
+            'last_name': 'Miller',
+            'user_status': UserProfile.PENDING,
+            'role': UserProfile.LEADER
+        }
+
+        # making change
+        data = {"role": UserProfile.VOLUNTEER, "status": UserProfile.INACTIVE}
+        endpoint = "/api/user/edit/" + str(user_data["user_id"])
+        response = self.client.post(endpoint, data, **self.bearer)
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+        self.assertEqual(json.loads(response.content), {"detail":"Not found."})
+
+    def test_edit_member_pending_status(self):
+        # before change
+        user_data = {
+            'user_id': 4,
+            'email':  'leaderPendingStatus@example.com',
+            'first_name': 'Caroline',
+            'last_name': 'Miller',
+            'user_status': UserProfile.PENDING,
+            'role': UserProfile.LEADER
+        }
+
+        self.check_member_before_change(data=user_data)
+
+        # making change
+        data = {"role": UserProfile.VOLUNTEER, "status": UserProfile.INACTIVE}
+        endpoint = "/api/user/edit/" + str(user_data["user_id"])
+        response = self.client.post(endpoint, data, **self.bearer)
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+        self.assertEqual(json.loads(response.content), {
+                         'error': 'User can not be edited because her status is pending.'})
+        # after change
+        self.check_member_after_change(user_data)
+
+    def test_edit_member_by_nondirector(self):
+        username = 'leader@example.com'
+        password = 'Password123'
+        access_token = self.get_token(username, password)
+        bearer = {
+            'HTTP_AUTHORIZATION': 'Bearer {}'.format(access_token)}
+        # before change
+        user_data = {
+            'user_id': 4,
+            'email':  'leaderPendingStatus@example.com',
+            'first_name': 'Caroline',
+            'last_name': 'Miller',
+            'user_status': UserProfile.PENDING,
+            'role': UserProfile.LEADER
+        }
+
+        self.check_member_before_change(data=user_data)
+
+        # making change
+        data = {"role": UserProfile.VOLUNTEER, "status": UserProfile.INACTIVE}
+        endpoint = "/api/user/edit/" + str(user_data["user_id"])
+        response = self.client.post(endpoint, data, **bearer)
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+        self.assertEqual(json.loads(response.content), {
+                         'detail': 'You do not have permission to perform this action.'})
+        # after change
+        self.check_member_after_change(user_data)
+
+
+    def test_edit_role_member_notallowed_role(self):
+        user_data = {
+            'user_id': 3,
+            'email':  'leader@example.com',
+            'first_name': 'Bruno',
+            'last_name': 'Clark',
+            'user_status': UserProfile.ACTIVE,
+            'role': UserProfile.LEADER
+        }
+        # before change
+        self.check_member_before_change(data=user_data)
+
+        # making change
+        data = {"role": "SOMEROLE", "status": UserProfile.INACTIVE}
+        response = self.client.post("/api/user/edit/3", data, **self.bearer)
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+        self.assertEqual(json.loads(response.content), {
+            'error': "User's role or status entered is empty or incorrect."})
+
+        # after change
+        self.check_member_after_change(user_data)
+
+    def test_edit_status_member_notallowed_status(self):
+        user_data = {
+            'user_id': 3,
+            'email':  'leader@example.com',
+            'first_name': 'Bruno',
+            'last_name': 'Clark',
+            'user_status': UserProfile.ACTIVE,
+            'role': UserProfile.LEADER
+        }
+        # before change
+        self.check_member_before_change(data=user_data)
+
+        # making change
+        data = {"role": UserProfile.VOLUNTEER, "status": "SOMESTATUS"}
+        response = self.client.post("/api/user/edit/3", data, **self.bearer)
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+        self.assertEqual(json.loads(response.content), {
+            'error': "User's role or status entered is empty or incorrect."})
+
+        # after change
+        self.check_member_after_change(user_data)
+
+
+    def test_empty_input(self):
+        user_data = {
+            'user_id': 3,
+            'email':  'leader@example.com',
+            'first_name': 'Bruno',
+            'last_name': 'Clark',
+            'user_status': UserProfile.ACTIVE,
+            'role': UserProfile.LEADER
+        }
+        # before change
+        self.check_member_before_change(data=user_data)
+
+        # making change
+        data = {"role": "", "status": ""}
+        response = self.client.post("/api/user/edit/3", data, **self.bearer)
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+        self.assertEqual(json.loads(response.content), {
+            'error': "User's role or status entered is empty or incorrect."})
+
+        # after change
+        self.check_member_after_change(user_data)
+
+    def test_edit_member(self):
+        user_data = {
+            'user_id': 3,
+            'email':  'leader@example.com',
+            'first_name': 'Bruno',
+            'last_name': 'Clark',
+            'user_status': UserProfile.ACTIVE,
+            'role': UserProfile.LEADER
+        }
+        # before change
+        self.check_member_before_change(data=user_data)
+
+        # making change
+        data = {"role": UserProfile.VOLUNTEER, "status": UserProfile.INACTIVE}
+        response = self.client.post("/api/user/edit/3", data, **self.bearer)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(json.loads(response.content), {
+                         'result': self.EXPECTED_MESSAGE})
+
+        # after change
+        user_data['role'] = UserProfile.VOLUNTEER
+        user_data['user_status'] = UserProfile.INACTIVE
+        self.check_member_after_change(user_data)
---
+++ api/wwcodesvtools/api/views/EditMemberView.py
@@ -0,0 +1,126 @@
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.exceptions import ValidationError
+from api.permissions import CanEditMember
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.generics import GenericAPIView
+from django.contrib.auth.models import User
+from django.shortcuts import get_object_or_404
+from drf_yasg.utils import swagger_auto_schema
+from drf_yasg import openapi
+import logging
+from api.models import UserProfile
+from api.serializers.EditMemberSerializer import EditMemberSerializer
+from api.serializers.UserProfileSerializer import UserProfileSerializer
+from django.db import transaction
+
+logger = logging.getLogger('django')
+
+
+class EditMemberView(GenericAPIView):
+    """
+    Edits the User and its related information from the database
+    """
+    permission_classes = [IsAuthenticated & CanEditMember]
+
+    ERROR_UPDATING_USER_PROFILE = 'Error updating user profile'
+    ERROR_INVALID_INPUT_ROLE_STATUS = "User's role or status entered is empty or incorrect."
+
+    post_response_schema = {
+        status.HTTP_200_OK: openapi.Response(
+            description="User edited successfully",
+            examples={
+                "application/json": {
+                    'result': 'User edited successfully',
+                }
+            }
+        ),
+        status.HTTP_404_NOT_FOUND: openapi.Response(
+            description="Error not found",
+            examples={
+                "application/json": {
+                    'detail': 'Not found.',
+                }
+            }
+        ),
+        status.HTTP_500_INTERNAL_SERVER_ERROR: openapi.Response(
+            description="Internal Server Error",
+            examples={
+                "application/json": {
+                    'error': 'Error editing the User'
+                }
+            }
+        ),
+        status.HTTP_403_FORBIDDEN: openapi.Response(
+            description="User's role or status entered is empty or incorrect.",
+            examples={
+                "application/json": {
+                    'error': "User's role or status entered is empty or incorrect."
+                }
+            }
+        ),
+    }
+
+    @swagger_auto_schema(responses=post_response_schema)
+    @transaction.atomic
+    def post(self, request, id):
+        user = get_object_or_404(User, id=id)
+        try:
+            user_row = UserProfile.objects.get(id=id)
+            if user_row.status == UserProfile.PENDING:
+                return Response({'error': 'User can not be edited because her status is pending.'}, status=status.HTTP_403_FORBIDDEN)
+
+            data = {}
+            updatable_fields = ['role', 'status']
+            for field in updatable_fields:
+                if field in request.data:
+                    data[field] = request.data[field]
+
+            serializer_role_status = EditMemberSerializer(id, data=data)
+            if serializer_role_status.is_valid():
+                # creating txn savepoint
+                sid = transaction.savepoint()
+                res_profile_status_role = self.edit_user_profile(id, data)
+                if res_profile_status_role:
+                    # all well,commit data to the db
+                    transaction.savepoint_commit(sid)
+                    logger.info(
+                        'EditMemberView: User data edited successfully')
+                    res_status = status.HTTP_200_OK
+                    error = None
+                else:
+                    if not res_profile_status_role:
+                        error = self.ERROR_UPDATING_USER_PROFILE
+                    # something went wrong, don't commit data to db,rollback txn
+                    transaction.savepoint_rollback(sid)
+                    res_status = status.HTTP_500_INTERNAL_SERVER_ERROR
+            else:
+                error = self.ERROR_INVALID_INPUT_ROLE_STATUS
+                res_status = status.HTTP_403_FORBIDDEN
+
+        except Exception as e:
+            error = self.ERROR_UPDATING_USER_PROFILE
+            logger.error(f'EditMemberView: {error}: {e}')
+            res_status = status.HTTP_403_FORBIDDEN
+
+        if (error is None and res_status == status.HTTP_200_OK):
+            return Response({'result': 'User edited successfully'}, status=status.HTTP_200_OK)
+        return Response({'error': error}, status=res_status)
+
+    def edit_user_profile(self, user_id, data):
+        try:
+            user_row = UserProfile.objects.get(user_id=user_id)
+            logger.debug(
+                f'row {user_row.user_id} : {user_row.role}  : {user_row.status}')
+            if user_row:
+                serializer_profile = UserProfileSerializer(user_row, data=data)
+                if serializer_profile.is_valid():
+                    serializer_profile.save()
+                    return True
+                else:
+                    logger.error(
+                        f'AddMemberView:Error updating user profile : {serializer_profile.errors}')
+                    return False
+        except UserProfile.DoesNotExist as e:
+            logger.error(f'AddMemberView:Error updating user profile : {e}')
+            return False

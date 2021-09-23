51fe1059307ea5a7737d7000eb0edd7c2d158fee
soomathy <sumathi.chari@gmail.com>
2021-09-22 17:33:18 -0700

    Updated requirements and settings to remove references to external library

--- api/wwcodesvtools/wwcodesvtools/settings.py
+++ api/wwcodesvtools/wwcodesvtools/settings.py
@@ -48,7 +48,6 @@ INSTALLED_APPS = [
     'rest_framework_simplejwt.token_blacklist',
     'drf_yasg',
     'corsheaders',
-    'django_filters'
 ]
 
 MIDDLEWARE = [
@@ -187,7 +186,6 @@ REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': (
         'rest_framework.permissions.IsAuthenticated',
     ),
-    'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
 }
 
 SIMPLE_JWT = {

94d68932ec9624c2c2622fcf10d3f416e04c8bd3
soomathy <sumathi.chari@gmail.com>
2021-09-22 17:32:01 -0700

    remove filters.py as not using external libraries

--- api/wwcodesvtools/api/filters.py
+++
@@ -1,9 +0,0 @@
-from django.contrib.auth.models import User
-import django_filters
-
-
-class UserProfileFilter(django_filters.FilterSet):
-
-    class Meta:
-        model = User
-        fields = ['userprofile__role', 'userprofile__status']

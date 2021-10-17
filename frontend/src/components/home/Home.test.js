516eb0c08c06e694637f0e88f5f21d75440d8f65
Emily Sheng <emily.s2442@gmail.com>
2021-10-17 14:36:24 -0700

    Added test for home and layout components. Typo fix data-testId => data-testid in password component.

---
+++ frontend/src/components/home/Home.test.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import Home from './Home';
+
+jest.mock('react-router-dom', () => ({
+    useHistory: () => ({
+      push: jest.fn()
+    })
+}));
+
+jest.mock('react', () => {
+    const ActualReact = require.requireActual('react');
+    return {
+        ...ActualReact,
+        useContext: () => ({
+            userInfo: {
+                email: 'director@example.com',
+                first_name: 'John',
+                id: 1,
+                last_name: 'Smith',
+                role: 'DIRECTOR'
+            },
+            handleRemoveAuth: jest.fn()
+        })
+    };
+});
+
+describe('Home', () => {
+    test('it renders without crashing', () => {
+        const { container } = render(<Home />);
+        expect(container).toMatchSnapshot();
+    });
+});

2b1cce957f2d8fc8a4d1819c6a203a07b45e41d1
Emily Sheng <emily.s2442@gmail.com>
2021-10-18 13:19:29 -0700

    Removed unnecessary imports

--- frontend/src/components/messagebox/MessageBox.test.js
+++ frontend/src/components/messagebox/MessageBox.test.js
@@ -1,7 +1,5 @@
 import React from 'react';
 import { render } from '@testing-library/react';
-import renderer from 'react-test-renderer';
-import 'jest-styled-components';
 import MessageBox from './MessageBox';
 
 describe('MessageBox', () => {

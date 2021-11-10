137531d8e74a27cd77526a83e7d1e5b7774bc4f0
cmmerritt <christiane.merritt@gmail.com>
2021-11-09 17:59:14 -0800

    reset pw form test now passes

--- frontend/src/components/resetpwform/ResetPasswordForm.test.js
+++ frontend/src/components/resetpwform/ResetPasswordForm.test.js
@@ -2,22 +2,16 @@ import React from "react";
 import { render, screen, act, fireEvent } from "@testing-library/react";
 import ConfirmResetPassword from "./ResetPasswordForm";
 import AuthProvider from "../../context/auth/AuthProvider";
-import WwcApi from "../../WwcApi";
-import { createMemoryHistory } from 'history';
-import { Router} from "react-router";
 
 jest.mock("react-router");
 jest.mock("../../WwcApi");
 
 test('it should render reset password form', () => {
-  const history = createMemoryHistory({initialEntries: ['/register?email=a@b.com&token=test']});
-        let loc = {search: "?email=a@b.com&token=test"};
+  let loc = {search: "?email=a@b.com&token=test"};
   const handleSetAuth = jest.fn();
   const { getByTestId } = render (
     <AuthProvider value={{handleSetAuth,}}> 
-      <Router history={history}>
-        <ConfirmResetPassword location={loc}/>
-      </Router>
+      <ConfirmResetPassword location={loc}/>
     </AuthProvider>
   )
 
